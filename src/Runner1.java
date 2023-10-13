9535d4d1d2ca082be5ac6957799962a7b05faec2
skif- <44skif99@gmail.com>
2023-10-13 21:15:31 +0300

    lesson1

---
+++ src/Runner1.java
@@ -0,0 +1,10 @@
+public class Runner1 {
+    public static void main(String[] args) {
+        //System - класс
+        //out - его открытое static поле. (PrintStream класс out)
+        //println - метод класса PrintStream
+        System.out.println("Hello Java!");//в конце сообщения переходит на новую строку
+        System.out.print("Hello Java!");//не переходит на новую строку
+        System.out.printf("Hello Java!");//для форматированного вывода(%d %.2f)
+    }
+}
