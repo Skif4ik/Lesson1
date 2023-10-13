61615b7c54f7d119952da308f72e592c189d1503
skif- <44skif99@gmail.com>
2023-10-13 21:54:15 +0300

    lesson1

---
+++ src/Runner2.java
@@ -0,0 +1,12 @@
+public class Runner2 {
+    //String[] args программа готова принимать массив строк
+    // в editConfiguration добавили в program arguments
+    // три строки Kolya Fedya "Brus Li"
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+        //System.out.println(args[3]); ошибка!, т.к. передано
+        //только три аргумента
+    }
+}
